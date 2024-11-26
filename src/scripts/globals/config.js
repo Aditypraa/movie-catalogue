eb6e81e85b49c01aec5bc7f6c643715c693dd007
Aditya Pratama <aditypraa@gmail.com>
2024-11-26 13:11:55 +0700

    Melakukan Request dari API

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'c3e3bbf35d5cb087a6b673f39d2181d1',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
